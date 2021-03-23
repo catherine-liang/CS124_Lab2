65f30a3d58802a3d74ef1516d8e6eef4f7e9c234
catherine-liang <63128398+catherine-liang@users.noreply.github.com>
2021-03-22 23:15:06 -0400

    Add files via upload

--- test_strassencl.py
+++ test_strassencl.py
@@ -1,8 +1,7 @@
-from strassen import *
-from datetime import datetime
-import sys
+from strassencl import *
 from random import randint
-from math import log2
+import sys
+from datetime import datetime
 
 start = int(sys.argv[1])
 end = int(sys.argv[2])
@@ -10,7 +9,6 @@ end = int(sys.argv[2])
 a = [[randint(-1, 1) for j in range(end)] for i in range(end + 1)]
 b = [[randint(-1, 1) for j in range(end)] for i in range(end + 1)]
 
-print("starting")
 splits = [ceil(end/2**i) for i in range(1, ceil(log(end,2)))]
 print(splits)
 for c in splits:
