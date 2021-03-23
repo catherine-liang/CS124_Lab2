c310848a40b683c94c381240abbcf3f82056b9e4
Nishant Mishra <54038104+nmishra459@users.noreply.github.com>
2021-03-22 23:46:18 -0400

    Update strassens_triangle.py

--- strassens_triangle.py
+++ strassens_triangle.py
@@ -1,22 +1,18 @@
-from math import ceil, floor
 from random import randint, uniform
+from math import ceil, floor
 import sys
 
-
-def add_matrices(a, b):
-    return [[x + y for x, y in zip(col_a, col_b)] for col_a, col_b in zip(a, b)]
-
-
-def subtract_matrices(a, b):
-    return [[x - y for x, y in zip(col_a, col_b)] for col_a, col_b in zip(a, b)]
-
-
-def multiply_matrices(a, b):
+def matmult(a, b):
     z_b = list(zip(*b))
     return [[sum(x * y for x, y in zip(a_row, b_col)) for b_col in z_b] for a_row in a]
 
+def matadd(a, b):
+    return [[x + y for x, y in zip(col_a, col_b)] for col_a, col_b in zip(a, b)]
 
-def read_file(filename, d):
+def matsub(a, b):
+    return [[x - y for x, y in zip(col_a, col_b)] for col_a, col_b in zip(a, b)]
+
+def readfile(filename, d):
     dims = range(d)
     a = [[0 for i in dims] for j in dims]
     b = [[0 for i in dims] for j in dims]
@@ -42,59 +38,53 @@ def read_file(filename, d):
 def strassens(a, b, crossover=2):
     n = len(a)
 
-    # base case
     if n <= crossover:
-        return multiply_matrices(a, b)
+        return matmult(a, b)
 
-    # recursive case
     else:
         range_n = range(n)
-        # if not a power of 2
         if n%2 != 0:
-            # find next power of 2
-            new_n = n + 1
+            nx = n + 1
+            #pad with zeros
+            #a = [a[i] + [0] for i in range_n] + [[0] * nx]
+            #b = [b[i] + [0] for i in range_n] + [[0] * nx]
 
-            # pad with zeroes
-            a = [a[i] + [0] for i in range_n] + [[0] * new_n]
-            b = [b[i] + [0] for i in range_n] + [[0] * new_n]
+            a = [a[i] + [0]] + [[0]]
+            b = [b[i] + [0]] + [[0]]
 
         else:
-            new_n = n
+            nx = n
 
-        # where to split the matrix
-        split = new_n//2
+        split = nx//2
         first_half = range(0, split)
-        second_half = range(split, new_n)
+        second_half = range(split, nx)
 
-        # define sub-matrices
         A = [a[i][:split] for i in first_half]
-        B = [a[i][split:new_n] for i in first_half]
+        B = [a[i][split:nx] for i in first_half]
         C = [a[i][:split] for i in second_half]
-        D = [a[i][split:new_n] for i in second_half]
+        D = [a[i][split:nx] for i in second_half]
         E = [b[i][:split] for i in first_half]
-        F = [b[i][split:new_n] for i in first_half]
+        F = [b[i][split:nx] for i in first_half]
         G = [b[i][:split] for i in second_half]
-        H = [b[i][split:new_n] for i in second_half]
+        H = [b[i][split:nx] for i in second_half]
 
-        # sub-multiplications
-        P1 = strassens(A, subtract_matrices(F, H), crossover=crossover)
-        P2 = strassens(add_matrices(A, B), H, crossover=crossover)
-        P3 = strassens(add_matrices(C, D), E, crossover=crossover)
-        P4 = strassens(D, subtract_matrices(G, E), crossover=crossover)
-        P5 = strassens(add_matrices(A, D), add_matrices(E, H), crossover=crossover)
-        P6 = strassens(subtract_matrices(B, D), add_matrices(G, H), crossover=crossover)
-        P7 = strassens(subtract_matrices(A, C), add_matrices(E, F), crossover=crossover)
+        P1 = strassens(A, matsub(F, H), crossover=crossover)
+        P2 = strassens(matadd(A, B), H, crossover=crossover)
+        P3 = strassens(matadd(C, D), E, crossover=crossover)
+        P4 = strassens(D, matsub(G, E), crossover=crossover)
+        P5 = strassens(matadd(A, D), matadd(E, H), crossover=crossover)
+        P6 = strassens(matsub(B, D), matadd(G, H), crossover=crossover)
+        P7 = strassens(matsub(A, C), matadd(E, F), crossover=crossover)
 
-        # combine results
-        result = list(map(lambda x,y:x+y, add_matrices(subtract_matrices(add_matrices(P5, P4), P2), P6), add_matrices(P1, P2)))
-        result.extend(list(map(lambda x,y:x+y, add_matrices(P3, P4), subtract_matrices(subtract_matrices(add_matrices(P5, P1), P3), P7))))
+        result = list(map(lambda x,y:x+y, matadd(matsub(matadd(P5, P4), P2), P6), matadd(P1, P2)))
+        result.extend(list(map(lambda x,y:x+y, matadd(P3, P4), matsub(matsub(matadd(P5, P1), P3), P7))))
         return [result[i][:n]for i in range_n]
 
 
 if __name__ == "__main__":
     dimension = 1024
 
-    p = 0.05
+    p = 0.04
 
 
     a = [ [0]*dimension for _ in range(dimension) ]
